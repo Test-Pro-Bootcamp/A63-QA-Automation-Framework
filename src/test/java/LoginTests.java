21bf44e759545bdc73889fa6174c0333324c8203
dhivyasankaran17 <157330214+dhivyasankaran17@users.noreply.github.com>
2024-10-02 20:20:26 -0400

    Registration Link
    Verify Registration Link

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -18,8 +20,12 @@ public class LoginTests extends BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://qa.koel.app/";
+        String regUrl = "https://qa.koel.app/registration";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+        WebElement registrationLink = driver.findElement(By.cssSelector("[href = 'registration']"));
+        registrationLink.click();
+        // compare if reg link is equal to current link
+        Assert.assertEquals(driver.getCurrentUrl(), regUrl);
         driver.quit();
     }
 }

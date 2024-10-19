dc665beac00e5b891eaddde9ccdadce18c54da2e
Issamkawtharani <issam.kawtharani@testpro.io>
2024-10-18 23:56:33 -0500

    Add Song to Playlist
    Add Song to Playlist

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,23 +1,25 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class Homework17 extends BaseTest {
     @Test
-    public void addSongToPlayList() throws InterruptedException {
-
+    public void addSongToPlaylist() throws InterruptedException {
         String expectedSongAddedMessage = "Added 1 song into \"Test Pro Playlist.\"";
-
         navigateToPage();
         provideEmail("issam.kawtharani@testpro.io");
         providePassword("Issam@testpro1");
         clickSubmit();
-        Thread.sleep(2000);;
+        Thread.sleep(2000);
         searchSong("Pluto");
         clickViewAllBtn();
         selectFirstSongResult();
         clickAddToBtn();
-        choosePlayList();
-        Assert.assertEquals(getAddToPlayListSuccessMsg(), expectedSongAddedMessage);
-
+        choosePlaylist();
+        Assert.assertEquals(getAddToPlaylistSuccessMsg(), expectedSongAddedMessage);
     }
 }

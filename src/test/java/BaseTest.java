8f3f3081820197e05449253a0e8cfa74adbcd26e
dhivyasankaran17 <157330214+dhivyasankaran17@users.noreply.github.com>
2024-10-23 20:47:17 -0400

    Homework24
    Grid implementation

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,11 +5,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -31,14 +38,8 @@ public class BaseTest {
 
     @BeforeMethod
     @Parameters({"BaseURL"})
-    public void launchClass(String BaseURL) {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        Map<String, Object> prefs = new HashMap<String, Object>();
-        prefs.put("profile.default_content_setting_values.notifications", 1);
-        //1-Allow, 2-Block, 0-default
-        options.setExperimentalOption("prefs", prefs);
-        driver = new ChromeDriver(options);
+    public void launchClass(String BaseURL) throws MalformedURLException {
+        driver = pickBrowser(System.getProperty("browser"));
 
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -55,5 +56,36 @@ public class BaseTest {
 
     public void navigateToPage(){
         driver.get(url);
+    }
+    public static WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://192.168.86.23:4444";
+
+        switch(browser){
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            case "Microsoft Edge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                return driver = new EdgeDriver(edgeOptions);
+            case "grid-edge":
+                caps.setCapability("browserName","MicrosoftEdge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+            case "grid-chrome":
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                Map<String, Object> prefs = new HashMap<String, Object>();
+                prefs.put("profile.default_content_setting_values.notifications", 1);
+                //1-Allow, 2-Block, 0-default
+                chromeOptions.setExperimentalOption("prefs", prefs);
+                return driver = new ChromeDriver(chromeOptions);
+
+        }
     }
 }
--- src/test/java/Homework24.java
+++ src/test/java/Homework24.java
@@ -3,7 +3,7 @@ import POM.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Homework23 extends BaseTest {
+public class Homework24 extends BaseTest {
 
     @Test
     public void renamePlaylist() throws InterruptedException{

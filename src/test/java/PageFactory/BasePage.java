a29fc40c972dc1ab9a924246d643ac8225c2d280
vickuta01 <victoria.mereneanu@testpro.io>
2024-10-25 12:38:15 -0500

    Update BasePage.java

--- src/test/java/PageFactory/BasePage.java
+++ src/test/java/PageFactory/BasePage.java
@@ -1,20 +1,12 @@
 package PageFactory;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
@@ -22,20 +14,22 @@ public class BasePage {
     WebDriver driver;
     WebDriverWait wait;
     Actions actions;
-
     public BasePage(WebDriver givenDriver) {
         driver = givenDriver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         PageFactory.initElements(driver, this);
     }
-
+    public WebElement findElement(WebElement webElement) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated((By) webElement));
+    }
     public void click(WebElement webElement){
-        webElement.click();
+        findElement(webElement).click();
     }
     public void doubleClick(WebElement webElement) {
-        actions.doubleClick(webElement).perform();
+        actions.doubleClick(findElement(webElement)).perform();
     }
     public void contextClick(WebElement webElement) {
-        actions.contextClick(webElement).perform();
+        actions.contextClick(findElement(webElement)).perform();
     }
+
 }

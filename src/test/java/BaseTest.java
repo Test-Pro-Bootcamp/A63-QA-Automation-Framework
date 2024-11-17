e27eeb6ade1924d3770edc1be62b09df39b5f9b6
Issamkawtharani <issam.kawtharani@testpro.io>
2024-11-17 14:51:02 -0600

    Lesson-25-Parallel-Testing

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.bouncycastle.oer.its.etsi102941.Url;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -18,9 +19,13 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
+import javax.print.DocFlavor;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
+import java.net.URLPermission;
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
 
 public class BaseTest {
@@ -29,19 +34,26 @@ public class BaseTest {
     public static WebDriverWait wait = null;
     public static Actions actions = null;
     public String newPlaylistName = "Sample Edited Playlist";
-
+    private static final ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
 
     @BeforeSuite
     static void setupClass() {
-        //WebDriverManager.chromedriver().setup();
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeMethod
     @Parameters({"BaseURL"})
+
+    public void setUpBrowser (String BaseURL) throws MalformedURLException {
+        threadDriver.set(pickBrowser (System.getProperty("browser")));
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        getDriver().manage().window().maximize();
+        url = BaseURL;
+        navigateToPage(BaseURL);
+    }
     public void launchBrowser(String BaseURL) throws MalformedURLException {
 
-        driver = pickBrowser(System.getProperty("browser"));
+      /*  driver = pickBrowser(System.getProperty("browser"));
         //added chromeOptions arguments below to fix websocket error
 
         //ChromeOptions options = new ChromeOptions();
@@ -53,16 +65,26 @@ public class BaseTest {
         wait = new WebDriverWait(driver , Duration.ofSeconds(10));
         actions = new Actions(driver);
         url = BaseURL;
-        navigateToPage();
+        navigateToPage(); */
     }
 
     @AfterMethod
 
-    public void closeBrowser(){ driver.quit(); }
-    public void navigateToPage(){
-        driver.get(url);
+
+    public void tearDown(){
+        threadDriver.get().close();
+        threadDriver.remove();
     }
 
+    public void closeBrowser(){ driver.quit(); }
+
+    public void navigateToPage(String url){
+        getDriver().get(url);
+    }
+
+    public static WebDriver getDriver(){
+        return threadDriver.get();
+    }
 
     public static WebDriver pickBrowser (String browser) throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
@@ -92,6 +114,9 @@ public class BaseTest {
                 caps.setCapability("browserName", "chrome");
                 return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
 
+            case "cloud":
+                return lambdaTest();
+
             default:
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
@@ -100,4 +125,21 @@ public class BaseTest {
         }
 
     }
+
+    public static WebDriver lambdaTest() throws MalformedURLException {
+        String hubURL = "https://hub.lambdatest.com/wd/hub";
+        ChromeOptions browserOptions = new ChromeOptions();
+        browserOptions.setPlatformName("Windows 10");
+        browserOptions.setBrowserVersion("130");
+        HashMap<String, Object> ltOptions = new HashMap<String, Object>();
+        ltOptions.put("username", "issam.kawtharani");
+        ltOptions.put("accessKey", "OqxUgyUe41hgs87BQkCDzqpyh7l6pCxyygRfPuN1xxlVz8khTO");
+        ltOptions.put("project", "Untitled");
+        ltOptions.put("name", "TestPro");
+        ltOptions.put("selenium_version", "4.0.0");
+        ltOptions.put("w3c", true);
+        browserOptions.setCapability("LT:Options", ltOptions);
+        return new RemoteWebDriver (new URL(hubURL), browserOptions);
+    }
+
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -7,11 +7,35 @@ public class LoginTests extends BaseTest {
     //Fluent interface example
     @Test
     public void loginValidEmailPassword() {
-        LoginPage loginPage = new LoginPage(driver);
-        HomePage homePage = new HomePage(driver);
+        //LoginPage loginPage = new LoginPage(driver);
+        //HomePage homePage = new HomePage(driver);
+        LoginPage loginPage = new LoginPage(getDriver());
+        HomePage homePage = new HomePage(getDriver());
         loginPage.provideEmail("issam.kawtharani@testpro.io")
                  .providePassword("Issam@testpro1")
                  .clickSubmit();
         Assert.assertTrue(homePage.isAvatarDisplayed());
+    }
+
+    @Test
+    public void loginInValidEmailPassword() {
+        String url = "https://qa.koel.app/";
+        //LoginPage loginPage = new LoginPage(driver);
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.provideEmail("invalidemail@testpro.io ")
+                .providePassword("wrongpassword@testpro1")
+                .clickSubmit();
+        Assert.assertEquals(getDriver().getCurrentUrl(), url);
+    }
+
+    @Test (enabled = false)
+    public void loginInEmptyEmailPassword() {
+        String url = "https://qa.koel.app/";
+        //LoginPage loginPage = new LoginPage(driver);
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.provideEmail(" ")
+                .providePassword("")
+                .clickSubmit();
+        Assert.assertEquals(getDriver().getCurrentUrl(), url);
     }
 }

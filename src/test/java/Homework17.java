6965d0a35cf7e63df48657b6965690d66c008cb5
Nazarchino <nazarchino@gmail.com>
2024-11-27 21:46:08 +0200

    Update Homework17.java

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,32 +1,38 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework17 extends BaseTest {
 
     @Test
-    public void addSongToPlaylist() {
+    public void addSongToPlaylist() throws InterruptedException {
         login("nazar@testpro.io", "Pomidor2115");
-        WebElement newPlaylist = driver.findElement(By.cssSelector("sidebar-create-playlist-btn"));
-        newPlaylist.click();
-        WebElement simplePlaylist = driver.findElement(By.cssSelector("playlist-context-menu-create-simple"));
-        simplePlaylist.click();
-        WebElement playlistName = driver.findElement(By.cssSelector("#playlists > form > input[type=text]"));
-        playlistName.sendKeys("Playlist 1");
-        playlistName.sendKeys(Keys.RETURN);
-        WebElement createdPlaylistText = driver.findElement(By.cssSelector("[class='success show']"));
-        Assert.assertTrue(createdPlaylistText.isDisplayed());
+//        WebElement newPlaylist = driver.findElement(By.cssSelector("sidebar-create-playlist-btn"));
+//        newPlaylist.click();
+//        WebElement simplePlaylist = driver.findElement(By.cssSelector("playlist-context-menu-create-simple"));
+//        simplePlaylist.click();
+//        WebElement playlistName = driver.findElement(By.cssSelector("#playlists > form > input[type=text]"));
+//        playlistName.sendKeys("Playlist 1");
+//        playlistName.sendKeys(Keys.RETURN);
+//        WebElement createdPlaylistText = driver.findElement(By.cssSelector("[class='success show']"));
+//        Assert.assertTrue(createdPlaylistText.isDisplayed());
+        int width = 1920;
+        int height = 1080;
+        Dimension dimension = new Dimension(width, height);
+        driver.manage().window().setSize(dimension);
         WebElement allSongs = driver.findElement(By.cssSelector("[href='#!/songs']"));
         allSongs.click();
+        Thread.sleep(500);
         WebElement testSong = driver.findElement(By.xpath("//*[@id='songsWrapper']/div/div/div[1]/table/tr[3]"));
         testSong.click();
-        WebElement addTo = driver.findElement(By.cssSelector(".btn-add-to"));
+        Thread.sleep(500);
+        WebElement addTo = driver.findElement(By.cssSelector("#songsWrapper > header > div.song-list-controls > span > button.btn-add-to"));
         addTo.click();
-        WebElement playlist1 = driver.findElement(By.cssSelector("#songsWrapper > header > div.song-list-controls > div > section.existing-playlists > ul > li.playlist"));
+        Thread.sleep(500);
+        WebElement playlist1 = driver.findElement(By.xpath("//*[@id='songsWrapper']/header/div[3]/div/section[1]/ul/li[5]"));
         playlist1.click();
-
+        Thread.sleep(500);
+        //WebElement songAdded = driver.findElement(By.cssSelector("[class=success show]"));
 
     }
 }
